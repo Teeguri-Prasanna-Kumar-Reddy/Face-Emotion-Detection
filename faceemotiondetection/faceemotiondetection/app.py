58828823dbf82adfcc5d6d46de7d961e50adfd4b
Teeguri_Prasanna_Kumar_Reddy <prasannakumar20131a4258@gmail.com>
2023-07-20 11:52:28 +0530

    All the files are added to the GitHub

---
+++ faceemotiondetection/faceemotiondetection/app.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 25 11:53:51 2023
+
+@author: sreej
+"""
+
+from flask import Flask,render_template,Response
+
+from Test import emotion
+
+detect = emotion()
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+   return render_template('index.html')
+
+def gen(detect):
+    
+    while True:
+        frame = detect.detect_emotion()
+        yield(b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+        
+
+@app.route('/predict')
+
+def predict():
+    return Response(gen(detect),mimetype='multipart/x-mixed-replace;boundary=frame')
+if __name__ == '__main__':
+   app.run()
